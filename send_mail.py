5f51c9ff4daa489175e28767cba9f001c56588d8
Christina Ruddle <ruddlec@tcd.ie>
2020-04-26 19:17:01 +0100

    Adding logging

--- send_mail.py
+++ send_mail.py
@@ -1,6 +1,11 @@
 import smtplib
+import logging
 
 from credentials import credentials
+
+
+logging.basicConfig(level=logging.INFO)
+
 
 gmail_user = "christina.mary.ruddle@gmail.com"
 gmail_password = credentials['gmail']
@@ -27,18 +32,22 @@ Subject: %s
 )
 
 
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.ehlo()
-# Upgrading to more secure connection
-server.starttls()
-server.ehlo()
-server.login(gmail_user, gmail_password)
-server.sendmail(
-    email_params['sent_from'],
-    email_params['to'],  
-    email_text
-)
-server.close()
+def send_email():
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    # Upgrading to more secure connection
+    server.starttls()
+    server.ehlo()
+    server.login(gmail_user, gmail_password)
+    logging.info("Sending email to {0}".format(email_params['to']))
+    server.sendmail(
+        email_params['sent_from'],
+        email_params['to'],  
+        email_text
+    )
+    server.close()
 
 
+if __name__ == '__main__':
+    send_email()
 
