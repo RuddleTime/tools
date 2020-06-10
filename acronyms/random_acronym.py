ce59a1734807d351cbd27f2ec7ae1c7ef12dd1f7
Christina Ruddle <ruddlec@tcd.ie>
2020-06-10 11:20:54 +0100

    Adding main and docstring

--- acronyms/random_acronym.py
+++ acronyms/random_acronym.py
@@ -1,11 +1,21 @@
 import random
 
-acronyms = open('acronyms.txt').read().splitlines()
 
-while True:
-    acronym = random.choice(acronyms)
-    acronym_and_meaning = acronym.split(" - ")
-    print(acronym_and_meaning[0], end = '')
-    input("")
-    print(acronym)
-    input("")
+def random_acronym(): 
+    """
+    Outputs random acronym read in from txt file.
+    On user keyboard input, meaning of acronym is printed.
+    """
+    acronyms = open('acronyms.txt').read().splitlines()
+
+    while True:
+        acronym = random.choice(acronyms)
+        acronym_and_meaning = acronym.split(" - ")
+        print(acronym_and_meaning[0], end = '')
+        input("")
+        print(acronym)
+        input("")
+
+
+if __name__=='__main__':
+    random_acronym() 
